5658ab4610e77581e50778094753cff20329fac4
Geir Gåsodden <geir.gasodden@gmail.com>
2026-04-14 06:57:20 +0200

    c legger til prettier vha copilot

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import storybook from "eslint-plugin-storybook";
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -16,7 +17,8 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
-  ...storybook.configs["flat/recommended"]
+  ...storybook.configs["flat/recommended"],
+  prettier,
 ]);
 
 export default eslintConfig;
